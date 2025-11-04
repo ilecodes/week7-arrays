8ff42675ac325fa29f3ec4781854bb64d0a03101
Ilhem Cherif <Cherif-I@ulster.ac.uk>
2025-11-04 11:00:38 +0300

    so many things done

--- MyArrayProject/Program.cs
+++ MyArrayProject/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System;
+using System.Security.Cryptography;
 
 namespace MyArrayProject
 {
@@ -10,6 +9,58 @@ namespace MyArrayProject
     {
         static void Main(string[] args)
         {
+            //task 2
+            char[] alphabet = new char[26];
+            //task 3
+            Console.WriteLine("Populate the array");
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                alphabet[i] = (char)(97 + i);
+                Console.WriteLine(alphabet[i]);
+            }
+            //task 4
+            Console.WriteLine("Printed in Reverse order");
+            for (int i = alphabet.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(alphabet[i]);
+            }
+            //task 5
+            Console.WriteLine("Modify the array");
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                alphabet[i] = (char)(65 + i);
+                Console.WriteLine(alphabet[i]);
+            }
+            // task 6
+            int[] fibonacci = {13,1,2,3,5,8,1,34,21,55};
+            // task 7
+            Console.WriteLine("Find Maximum");
+            int max = 0;
+            for (int i = 0;i < fibonacci.Length; i++)
+            {
+                if (fibonacci[i] > max) 
+                { max = fibonacci[i]; }
+            }
+            Console.WriteLine("Largest Value in Array:" + max);
+            // task 8
+            Console.WriteLine("Sort the Array");
+            Array.Sort(fibonacci);
+            foreach (int i in fibonacci)
+            {
+                Console.WriteLine(i);
+            }
+            // task 9
+            Console.WriteLine("Search for 34");
+            int index = -1;
+            int numtoFind = 34;
+            for (int i =0 ; i < fibonacci.Length; i++)
+            {
+                if (fibonacci[i]== numtoFind)
+                {
+                    index = i;
+                }
+            }
+            Console.WriteLine("The num is found at " + index);
         }
     }
 }
---
+++ MyListProject/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace MyListProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // task 2
+            List<string> months = new List<string>();
+            //task 3
+            Console.WriteLine("Populating the Array");
+            months.Add("January");
+            months.Add("February");
+            months.Add("March");
+            months.Add("April");
+            months.Add("May");
+            months.Add("June");
+            months.Add("July");
+            months.Add("August");
+            months.Add("September");
+            months.Add("October");
+            months.Add("November");
+            months.Add("December");
+            foreach (string month in months)
+            {
+                Console.WriteLine(month);
+            }
+            //task 4
+            Console.WriteLine("Convert list to array");
+            string[] monthNames = months.ToArray();
+            for (int i = 0; i < monthNames.Length; i++)
+            {
+                Console.WriteLine(monthNames[i]);
+            }
+            // task 5
+            Console.WriteLine("Remove from the list");
+            int numberM = months.Count;
+            Console.WriteLine("Length before: "+ numberM);
+            months.Remove("September");
+            months.Remove("November");
+            numberM = months.Count;
+            Console.WriteLine("Length after: " + numberM);
+            //task 6
+            Console.WriteLine("Search the list");
+            string monthtoFind = "December";
+            int index = months.IndexOf(monthtoFind);
+            Console.WriteLine("The position of month: " + index);
+        }
+    }
+}
